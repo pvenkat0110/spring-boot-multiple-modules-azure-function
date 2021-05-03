5e640543a8685e5e0b5ff84a11429fc0818978f6
“Venkat <venkat.reddy@wolkensoftware.com>
2021-05-03 14:55:16 +0530

    pom fix

--- src/test/java/com/azure/multimodulefunctions/MultiModuleFunctionsApplicationTests.java
+++ src/test/java/com/azure/multimodulefunctions/MultiModuleFunctionsApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MultiModuleFunctionsApplicationTests {
 
+	
 	@Test
-	void contextLoads() {
+	void sampleTest()
+	{
+		System.out.println("Do nothing");
 	}
 
 }
